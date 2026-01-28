3e83b9a457c79646ff45483f4d3fda9b2705580f
amstrd-cpc <justikerra@gmail.com>
2026-01-28 21:10:23 +0400

    switching things up to work

--- db/__init__.py
+++ db/__init__.py
@@ -1,1 +1,10 @@
 
+from db.connection import get_db, get_inventory_db, get_sales_db
+from db.migrations import migrate
+
+
+def init_db() -> None:
+    migrate()
+
+
+__all__ = ["get_db", "get_inventory_db", "get_sales_db", "init_db"]
